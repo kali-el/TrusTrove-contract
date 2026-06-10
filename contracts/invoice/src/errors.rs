15e7f1d3cfbcb8e15637f2baf9c9342585ac2cac
K1NGD4VID <adesanyafuhad5@gmail.com>
2026-06-10 22:17:48 +0100

    feat(invoice): define InvoiceError contracterror enum in errors.rs

---
+++ contracts/invoice/src/errors.rs
@@ -0,0 +1,18 @@
+use soroban_sdk::contracterror;
+
+#[contracterror]
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum InvoiceError {
+    AlreadyInitialized = 1,
+    NotFound = 2,
+    NotAuthorized = 3,
+    IssuerNotVerified = 4,
+    BuyerNotVerified = 5,
+    InvalidFaceValue = 6,
+    InvalidDueDate = 7,
+    InvalidStatusTransition = 8,
+    DiscountTooHigh = 9,
+    AlreadyConfirmed = 10,
+    DueDateNotPassed = 11,
+    InsufficientRepayment = 12,
+}
